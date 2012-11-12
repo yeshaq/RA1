ece48ea0d43fb344bfef514cb6530494cb76473c
Yossof Eshaq <yeshaq@yossof-UX31E.(none)>
2012-11-12 15:48:25 +0100

    caclulating nnpdf uncert

--- pdfWeights/ratio.py
+++ pdfWeights/ratio.py
@@ -11,7 +11,8 @@ canvas.SetTicky()
 
 models = ["T2bb"]#,"T1bbbb"]
 HTbins = ["275"]#,"275_scaled","325_scaled","375"])
-pdfSets = [["genMSTW2008nlo68cl", 41], ["gencteq66", 45]]#,["genNNPDF20", 31]]
+pdfSets = [["genMSTW2008nlo68cl", 41], ["gencteq66", 45],["genNNPDF20", 30]]
+#pdfSets = [["genNNPDF20", 30]]
 
 for model in models :
 
@@ -34,44 +35,62 @@ for model in models :
             result.SetTitle(";m_{parent} (GeV);m_{LSP} (GeV);ratio") 
             result.SetMarkerStyle(20)
             result.SetStats(False)
-            
-            for xbin in range(denHist[0].GetXaxis().GetNbins()) :
+
+            for xbin in range(denHist[0].GetXaxis().GetNbins()) :   # ISR cut-off
                 for ybin in range(denHist[0].GetYaxis().GetNbins()) :
                     if (ybin*25 > xbin*25 - 175) or (xbin*25 <= 300)  : result.SetBinContent(xbin,ybin,0.0) 
+            
+            if not pdfSet[0] == "genNNPDF20" : 
 
-
-            for xbin in range(denHist[0].GetXaxis().GetNbins()) :
-                for ybin in range(denHist[0].GetYaxis().GetNbins()) :
-                    if (result.GetBinContent(xbin,ybin)) == 0.0 : continue
-                    centralAcc = denHist[0].GetBinContent(xbin,ybin)
-                    dPlus = 0.
-                    dMinus = 0.
-                    #wp = 0.
-                    #wm = 5.
-                    if centralAcc == 0 : continue 
-                    for i in range(pdfSet[1]/2) :
-                        wp = denHist[2*i+1].GetBinContent(xbin,ybin)/denHist[0].GetBinContent(xbin,ybin) - 1
-                        wm = denHist[2*i+2].GetBinContent(xbin,ybin)/denHist[0].GetBinContent(xbin,ybin) - 1
-                        if (wp>wm) :
-                            if (wp<0.) : wp = 0.
-                            if (wm>0.) : wm = 0.
-                            dPlus  += wp*wp
-                            dMinus += wm*wm
-                        if (wp<wm) :
-                            if (wm<0.) : wm = 0.
-                            if (wp>0.) : wp = 0.
-                            dPlus  += wm*wm
-                            dMinus += wp*wp
-
-                    if (dPlus>0)  : dPlus = math.sqrt(dPlus)
-                    if (dMinus>0) : dMinus = math.sqrt(dMinus)
-                    if denHist[0].GetBinContent(xbin,ybin) > 0  : 
-                        print "---------------------------------------------------------------------------------------------"
-                        print "centr. accept = %.3f"%(100*centralAcc)
-                        print "ie. %.3f relative variation with respect to original PDF"%((100*(centralAcc-numHist.GetBinContent(xbin,ybin))/numHist.GetBinContent(xbin,ybin)))
-                        result.SetBinContent(xbin,ybin,(100*(centralAcc-numHist.GetBinContent(xbin,ybin))/numHist.GetBinContent(xbin,ybin)))
-                        print "Relative uncertianty with respect to central member: +%.3f / -%.3f"%(100*dPlus,100*dMinus)
-
+                for xbin in range(denHist[0].GetXaxis().GetNbins()) :
+                    for ybin in range(denHist[0].GetYaxis().GetNbins()) :
+                        if (result.GetBinContent(xbin,ybin)) == 0.0 : continue
+                        centralAcc = denHist[0].GetBinContent(xbin,ybin)
+                        dPlus = 0.
+                        dMinus = 0.
+                        if centralAcc == 0 : continue 
+                        for i in range(pdfSet[1]/2) :
+                            wp = denHist[2*i+1].GetBinContent(xbin,ybin)/denHist[0].GetBinContent(xbin,ybin) - 1
+                            wm = denHist[2*i+2].GetBinContent(xbin,ybin)/denHist[0].GetBinContent(xbin,ybin) - 1
+                            if (wp>wm) :
+                                if (wp<0.) : wp = 0.
+                                if (wm>0.) : wm = 0.
+                                dPlus  += wp*wp
+                                dMinus += wm*wm
+                            if (wp<wm) :
+                                if (wm<0.) : wm = 0.
+                                if (wp>0.) : wp = 0.
+                                dPlus  += wm*wm
+                                dMinus += wp*wp
+                        if (dPlus>0)  : dPlus = math.sqrt(dPlus)
+                        if (dMinus>0) : dMinus = math.sqrt(dMinus)
+                        if denHist[0].GetBinContent(xbin,ybin) > 0  : 
+                            print "---------------------------------------------------------------------------------------------"
+                            print "centr. accept = %.2f%%"%(100*centralAcc)
+                            print "ie. %.2f%% relative variation with respect to original PDF"%((100*(centralAcc-numHist.GetBinContent(xbin,ybin))/numHist.GetBinContent(xbin,ybin)))
+                            result.SetBinContent(xbin,ybin,(100*(centralAcc-numHist.GetBinContent(xbin,ybin))/numHist.GetBinContent(xbin,ybin)))
+                            print "Relative uncertianty with respect to central member: +%.2f%% / -%.2f%% "%(100*dPlus,100*dMinus)
+                            
+            else :
+                print pdfSet[0]
+                for xbin in range(denHist[0].GetXaxis().GetNbins()) :
+                    for ybin in range(denHist[0].GetYaxis().GetNbins()) :
+                        if (result.GetBinContent(xbin,ybin)) == 0.0 : continue
+                        mean_accept = 0
+                        sq_std_accept = 0
+                        std_accept = 0
+                        for i in range(pdfSet[1]) :
+                            mean_accept += denHist[i].GetBinContent(xbin,ybin)/pdfSet[1]
+                                                        
+                        for i in range(pdfSet[1]) :
+                            sq_std_accept += (denHist[i].GetBinContent(xbin,ybin)-mean_accept)*(denHist[i].GetBinContent(xbin,ybin)-mean_accept)/(pdfSet[1]-1)  
+                        std_accept = math.sqrt(sq_std_accept)
+                        print "mean accept = %.2f%%"%(100*mean_accept)
+                        print "ie. %.2f%% relative variation with respect to original PDF"%((100*(mean_accept-numHist.GetBinContent(xbin,ybin))/numHist.GetBinContent(xbin,ybin)))
+                        result.SetBinContent(xbin,ybin,(100*(mean_accept-numHist.GetBinContent(xbin,ybin))/numHist.GetBinContent(xbin,ybin)))
+                        print "Relative uncertianty with respect to central member: +- %.2f%%"%(100*std_accept)
+                        
+                
 
             if "T1bbbb" in model :
                 result.SetMaximum(1.2)
