c3505324ee942e6fd29b42c66c425fdd60655962
Yossof Eshaq <yeshaq@yossof-UX31E.(none)>
2012-12-16 21:50:39 +0100

    Caclulate acceptance for all pdf variations

--- pdfWeights/inclusive_acceptance.py
+++ pdfWeights/inclusive_acceptance.py
@@ -1,6 +1,15 @@
 import ROOT as r
 import common as c 
 
+def numerAndDenom(histName, befDir,afDir) :
+    d = {}
+    beforeHist = befDir.Get(histName) 
+    d["before"] = beforeHist.Clone()
+    afterHist = afDir.Get(histName)
+    d["after"] = afterHist.Clone()
+    return d
+
+
 for weight in c.weights :
     if weight == "" :
         for modAndPdf in c.mods_and_pdfs :
@@ -34,17 +43,13 @@ for weight in c.weights :
                     afterHist =  r.TH2D("nEvents_after","nEvents",81,0,2025,81,0,2025)
                     infile = r.TFile(rtfile,"READ")
                     befdir = infile.GetDirectory("master/progressPrinter/label/scanHistogrammer/")
-                    befkeylist = befdir.GetListOfKeys()        
-                    for key in befkeylist :
-                        if key.ReadObj().GetName() == "nEvents_%s_0"%(pdfSet):
-                            before = key.ReadObj()
-                            beforeHist = before.Clone()
+                    befkeylist = befdir.GetListOfKeys()
+                    outfile = r.TFile("output/acc_%s_%s_%s.root"%(modAndPdf[0],ht[0],pdfSet),"RECREATE") 
                     afdir = infile.GetDirectory(ht[1])
                     afkeylist = afdir.GetListOfKeys()
-                    for key in afkeylist :
-                        if key.ReadObj().GetName() == "nEvents_%s_0"%(pdfSet):
-                            after = key.ReadObj()
-                            afterHist = after.Clone()  
-                    afterHist.Divide(beforeHist)
-                    outfile = r.TFile("output/acc_%s_%s_%s.root"%(modAndPdf[0],ht[0],pdfSet),"RECREATE") 
-                    afterHist.Write()
+                    histList = ["nEvents_%s_%s"%(pdfSet,i) for i in range(c.nPdfDict[pdfSet])]
+                    for hist in histList :
+                         histos = numerAndDenom(hist,befdir,afdir)
+                         result = histos["after"].Clone()
+                         result.Divide(histos["before"])
+                         result.Write()
