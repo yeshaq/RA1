dbccfd4a74608b2ac4474ad3e4452305936bd377
yeshaq <yossofeshaq@gmail.com>
2013-04-30 10:42:10 +0200

    adding common functions

--- pdfWeights/common.py
+++ pdfWeights/common.py
@@ -1,3 +1,6 @@
+import ROOT as r
+
+
 version = ["v1","v2","v3","v4","v5"][4]
 
 afadd_275 = "master/progressPrinter/label/scanHistogrammer/monster/hbheNoise/multiplicity/multiplicity/l1Filter/physicsDeclaredFilter/lowestUnPrescaledTriggerFilter/jetPtSelector/jetPtSelector/jetEtaSelector/value/value/multiplicity/multiplicity/multiplicity/multiplicity/multiplicity/histogrammer/value/deadEcalFilter/cleanJetHtMhtHistogrammer/alphaHistogrammer/histogrammer/value/value/histogrammer/histogrammer/histogrammer/cleanJetHtMhtHistogrammer/histogrammer/histogrammer/histogrammer/label/scanHistogrammer/"
@@ -46,5 +49,82 @@ for bJet in bJets :
 dirNames.append("smsScan_ge4b_ge4j_AlphaT55_375")
 
 nPdfDict = {"gencteq66":45,"genMSTW2008nlo68cl":41,"genct10":53,"genNNPDF21":101}
-#nPdfDict = {"gencteq66":45,"genMSTW2008nlo68cl":3,"genct10":3,"genNNPDF21":3}
 
+def shift2DHistos(hist, shiftX = 0.0 , shiftY = 0.0, rebinX = 0.0, rebinY = 0.0, extraBins = 0) :
+
+
+    histName = hist.GetName()
+
+    histTitle = hist.GetTitle()
+    zMax = hist.GetMaximum()
+    zMin = hist.GetMinimum()
+    if not rebinX == 0.0 :
+        hist.Rebin2D(rebinX,rebinY)
+
+    xAxis = hist.GetXaxis()
+    yAxis = hist.GetYaxis()
+
+    nXbins = xAxis.GetNbins()
+    nYbins = yAxis.GetNbins()
+
+    xMin = xAxis.GetXmin()
+    yMin = yAxis.GetXmin()
+    
+    xMax = xAxis.GetXmax()
+    yMax = yAxis.GetXmax()
+    
+    dXbin = (xMax - xMin) / nXbins
+    dYbin = (yMax - yMin) / nYbins
+    
+    shiftedXmin = xMin + shiftX*dXbin
+    shiftedXmax = xMax + shiftX*dXbin
+    
+    shiftedYmin = yMin + shiftY*dYbin
+    shiftedYmax = yMax + shiftY*dYbin
+	
+
+    shiftedHist = r.TH2D(histName, histTitle, nXbins, shiftedXmin, shiftedXmax, nYbins, shiftedYmin, shiftedYmax + extraBins)
+    for xbin in range(nXbins) :
+        for ybin in range(nYbins) :
+            shiftedHist.SetBinContent(xbin+1,ybin+1,hist.GetBinContent(xbin+1,ybin+1))
+    shiftedHist.SetMinimum(zMin)
+    shiftedHist.SetMaximum(zMax)
+    return shiftedHist
+
+
+
+def resizeHisto(hist, rnXbins, loX, hiX, rnYbins, loY, hiY) :
+
+    histName = hist.GetName()
+    histTitle = hist.GetTitle()
+    zMax = hist.GetMaximum()
+    zMin = hist.GetMinimum()
+
+    xAxis = hist.GetXaxis()
+    yAxis = hist.GetYaxis()
+
+    nXbins = xAxis.GetNbins()
+    nYbins = yAxis.GetNbins()
+
+    xMin = xAxis.GetXmin()
+    yMin = yAxis.GetXmin()
+    
+    xMax = xAxis.GetXmax()
+    yMax = yAxis.GetXmax()
+
+    dXbin = (xMax - xMin) / nXbins
+    dYbin = (yMax - yMin) / nYbins
+
+    deltaLoXbins = (loX - xMin) / dXbin
+    deltaLoYbins = (loY - yMin) / dYbin
+
+ 
+    resizednXbins = (hiX - loX) / dXbin
+    resizednYbins = (hiY - loY) / dYbin
+    resizedHist = r.TH2D(histName, histTitle, rnXbins, loX, hiX, rnYbins, loY, hiY)
+    for xbin in range(nXbins) :
+        for ybin in range(nYbins) :
+            resizedHist.SetBinContent(xbin+1 ,ybin+1, hist.GetBinContent(xbin+(1+int(deltaLoXbins)), ybin+1 + int(deltaLoYbins)))
+    resizedHist.SetMinimum(zMin)
+    resizedHist.SetMaximum(zMax)
+    return resizedHist
